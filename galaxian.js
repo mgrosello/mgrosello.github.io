4c28ccb4711ec17bcd74164f171e4ab83c68b015
migar <migar@DESKTOP-2PSH5VT>
2021-05-07 08:41:56 +0200

    'compilar'

--- galaxian.js
+++ galaxian.js
@@ -187,7 +187,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/gfx/everything.png", "start": 0, "end": 7299, "audio": 0}], "remote_package_size": 7299, "package_uuid": "6753c834-1c1d-49da-b64b-3b671ee9d2af"});
+   loadPackage({"files": [{"filename": "/gfx/everything.png", "start": 0, "end": 7299, "audio": 0}], "remote_package_size": 7299, "package_uuid": "e04ffe52-ac78-432f-95fb-5042a29f0b12"});
   
   })();
   
